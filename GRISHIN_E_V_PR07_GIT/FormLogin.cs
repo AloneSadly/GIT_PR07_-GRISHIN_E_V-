174fdd5ae41a0d4cd5e12ad4775458e112bf4d0b
AloneSadly <foxer196031@gmail.com>
2025-10-24 23:04:54 +0300

    Update FormLogin.cs

--- GRISHIN_E_V_PR07_GIT/FormLogin.cs
+++ GRISHIN_E_V_PR07_GIT/FormLogin.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
@@ -10,11 +10,33 @@ using System.Windows.Forms;
 
 namespace GRISHIN_E_V_PR07_GIT
 {
-    public partial class Form1 : Form
+    public partial class FormLogin : Form
     {
-        public Form1()
+        public FormLogin()
         {
             InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            string login = txtLogin.Text;
+            string password = txtPassword.Text;
+
+            if (login == "admin" && password == "12345")
+            {
+                FormAdmin formAdmin = new FormAdmin();
+                formAdmin.Show();
+                this.Hide();
+            }
+            else
+            {
+                MessageBox.Show("Неверный логин или пароль!!");
+            }
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
         }
     }
 }
